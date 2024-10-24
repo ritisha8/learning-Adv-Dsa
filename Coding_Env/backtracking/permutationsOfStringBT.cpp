21685d0ef201e1943e6ccbcfa99ca6c387173dd7
ritisha8 <khuranaritisha4@gmail.com>
2024-10-25 00:23:49 +0530

    15

--- Coding_Env/backtracking/permutationsOfStringBT.cpp
+++ Coding_Env/backtracking/permutationsOfStringBT.cpp
@@ -17,10 +17,10 @@ void solve(string &s,int start,vector<string>&v){
     if(start==s.size()-1){  // when start pointer starts pointer to the last node, then we get  our ans instring according to backtraking tree
         v.push_back(s);  // before we have input putut here we have the string only becase we are making the hcanges in place 
     }
-    unordered_set<char>st;
+    unordered_set<char>st;  // tp control recrusion
     for(int i=start;i<s.length();i++){  // we aremoving froms atrt to end end ofateing  because we can make chnages from satrt to end ony , any string befre start has been fixed and desion for thta has been taken
-        if(st.find(s[start])==st.end()){
-            st.insert(s[start]);
+        if(st.find(s[i])==st.end()){  //we are starting from start because the choices we further need tomake will be from start to end , the choices of swap
+            st.insert(s[i]);
             swap(s[start],s[i]);   // acording to BT tree
             solve(s,start+1,v);
             swap(s[start],s[i]);  // backtraking step
