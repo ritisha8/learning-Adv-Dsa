74661b63e5b7e0567713e3b6bca33f9d853a260a
ritisha8 <khuranaritisha4@gmail.com>
2024-11-16 17:42:36 +0530

    GRAPH : DFS (Commented)

--- Coding_Env/graph/dfs.cpp
+++ Coding_Env/graph/dfs.cpp
@@ -1,3 +1,14 @@
+//herewhat we do is first we make the starting node as viosited and
+// then we push it intoans vector, thenwe iterate thrpugh each of its
+// neigbours and then we recursively call df for its neighbour, and
+// then we make the neigbour visted and then we push it into ans vector, 
+//and then we explore tehneighborus neioghbour , and then we recurisvely call
+// dfs on its neighbours whoare not allready visietd, then when all the 
+//nodes are visietd , we bactrak and then wesee if there aare any other
+// neighbours not visted of that call, if no we backtrack again until we raech 
+//a point when all recursive callsare bactracked and all nodes are visisted 
+//at each call 
+
 #include<bits/stdc++.h>
 using namespace std;
  void dfs(int node,vector<vector<int>>adj,int vis[],vector<int>&ls){
