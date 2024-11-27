7f8d525bbbfdc91e42ac8727b1b388926d5e975d
ritisha8 <khuranaritisha4@gmail.com>
2024-11-27 23:13:02 +0530

    SDE SHEET: Find the duplicate in an array of N+1 integers(BRUTE)

--- Coding_Env/sdeSheet/DuplicateInN+1.cpp
+++ Coding_Env/sdeSheet/DuplicateInN+1.cpp
@@ -1,2 +1,27 @@
+// brute force
+//TC: O(nlogn) + O(n); where n is the size of array
 #include<bits/stdc++.h>
-usign namesspacestd;
+using namespace std;
+int solve(vector<int>&arr){
+    // step1 : sort the array
+    int num=0;
+    sort(arr.begin(),arr.end());
+    //step2 ; trversing linearly to check if at any point arr[i]==arr[i+1] that is its next element
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==arr[i+1]){
+         num=arr[i];
+         break;
+        }
+    }
+    return num;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<solve(arr);
+    return 0;
+}
