cecced18a90ea1d9fdcdc02294ad8d32321ac146
ritisha8 <khuranaritisha4@gmail.com>
2024-12-10 11:37:33 +0530

    SDE SHEET: MAJORITY II

--- Coding_Env/dp/knapsack.cpp
+++ Coding_Env/dp/knapsack.cpp
@@ -12,17 +12,18 @@ int knapsack(vector<int> wt,vector<int> val,int W,int n){
     }
      //how dowe do recursion? by making input small? where we need to do recrusion, wherever there are choices abd we need to make a decision
     // hee we have choices whether to out an item in knapsack or not accordign ot its weight , we made a decsion tree according ot that if wt ofitem is more than knapsack cap, then ofc it will not be included, if wt of item is less than or = Cap then we have 2 chocies whther toincludde it or not 
-    // remember how we did in sorting array mid of stack , we make input small by taking the alste llemt out and then storing it somewhere  doign a  recursibve call for reduced input , here we dothe same 
+    // remember how we did in sorting array mid of stack , we make input small by taking the last ellemt out and then storing it somewhere  doign a  recursibve call for reduced input , here we dothe same 
     // we jsut dont eliminate last elemnt directly we make a decsion for the last eelemnt whther to include it or not according to decison tree
-    //now we memorise it , memorise means we will store it in matrix in order to store the output of recrusive call, so that if the same rec call is made in any other part of recusrive tree then it it will just take value form the matrix andf then extra overhead will be reduced
+    //now we memorise it , memorise means we will store it in matrix in order to store the output of recrusive call, so that if the same rec call is made in any other part of recusrive tree then it will just take value form the matrix andf then extra overhead will be reduced
     if(t[n][W]!=-1){  // we check now if there is -1 then this condition doesnt execute and recursive call is made noramlly and ans is stored in 
     // but if t[n][W]!=-1 then it must contain max profit of same recursion call that happened before and then we just simply return it
         return t[n][W];
     }
     if(wt[n-1]<=W){  // here we check if wt of item is smaller than cap
-        return t[n][W]=max(val[n-1]+knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1));  // then we took two choices one of including val[n-1], when we include n-1 in knapsack then ofc its cap for further checing will decrease because that space is occupied by the wt of this elemnt we chose , and another choice we make of not inluding the ellemtn and just dogn a recyrsive call for n-1 elelmtns , and now we put bothinto max function , because we want o return the ,ax profit , as we have two choices the ,ax of 2 will be returned
+        return t[n][W]=max(val[n-1]+knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1));  // then we took two choices one of including val[n-1], when we include n-1 in knapsack then ofc its cap for further checing will decrease because that space is occupied by the wt of this elemnt we chose ,
+        // and another choice we make of not inluding the ellemtn and just dogn a recyrsive call for n-1 elelmtns , and now we put bothinto max function , because we want o return the ,ax profit , as we have two choices the ,ax of 2 will be returned
     }
-    if(wt[n-1>W]) //then we check if  weight of n-1 ellemtns is greater than cap then, simply dont include the eelemnt swe simply make a recursie call to one choicethatw e have not nt including
+    else//then we check if  weight of n-1 ellemtns is greater than cap then, simply dont include the eelemnt swe simply make a recursie call to one choicethatw e have not nt including
     {
         return t[n][W]=knapsack(wt,val,W,n-1); // 
     }
---
+++ Coding_Env/sdeSheet/majority2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+//why would ans have only 2 ellments because you see , if size of vector is 6 , n/3 would be 2, so we need ellemtn size min to be 3 right? to be greater than 2, yes
+// so lets se how many elemtns can we have of 3 size in 6 size array , 3+3 , only 2 right , we cant have not even a single one at all.
+//so only  elemnt at max,we an have in 6 size array, lets see in 8 size array, n/3 would be 2, so here if we do 3+3+3 exceeeds the size fo array right after adding the last 3
+// now lets see with 9 , n/3 is 3,so 4 size count we need min , 4+4+4 is not possible only 2 4s are possible after that it would cross the count
+vector<int> solve(vector<int> &v){
+    vector<int> ans;
+     for(int i=0;i<v.size();i++){
+        if(ans.size()==0 || ans[0]!=v[i]){   
+            // if ans size is 0 or ans first ellemt is not equal to v[i], 
+            //we check the whole array if for teh coutn of v[i], other wise we just sip the iteration if the ans size is neither 0 nor its first eleltn is!=v[i], which menas if foudn equal we skip the oteration
+            // because ans will have only 2 elemnts
+          int cnt=0;   // then we are chechking for count here so we make cnt as 0 
+          for(int j=0;j<v.size();j++){// then for counting we run a j loop 
+            if(v[i]==v[j]) cnt++;  // we tarverse from the start if v[i] == v[j], simply increment the count
+          }
+          if(cnt > (v.size()/3)){  // after one checking of whole array is done for gettign coutn
+          // we check if the count is found after traversing the whole array is greater than n/3 if yes, add it to teh ans vector
+            ans.push_back(v[i]);
+          }
+        }
+        if(ans.size()==2) break;  // an after each iteration i is done we check whenevr ans ectr size turns 2 we break out of loop , becausew we dont need to check further this is teh ans
+     }
+     return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int>m=solve(v);
+    for(int i=0;i<m.size();i++){
+        cout<<m[i]<<" ";
+    }
+    return 0;
+}
+
